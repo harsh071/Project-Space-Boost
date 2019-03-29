e4a2f321c05c76b5c30612aa8050c10a42a03dab
Harsh Patel <hypharsh@gmail.com>
2019-03-28 21:10:07 -0500

    Thrust added.
    Now the rocket can fly in all directions !!

---
+++ RocketGame/Assets/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();   
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidbody.AddRelativeForce(Vector3.up);          
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(-Vector3.right);
+        
+        }
+    }
+
+}
